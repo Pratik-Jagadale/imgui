b2faa1d49e8ce32fadaa39ae82d56f4af8d93f4e
Pratik Jagadale <jagadalepratik54@gmail.com>
2024-02-11 01:14:00 +0530

    Adding Premake

---
+++ premake5.lua
@@ -0,0 +1,51 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("Bin/" .. outputdir)
+	objdir ("Bin-Int/" .. outputdir)
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp",
+		"imgui_tables.cpp",
+		"backends/imgui_impl_opengl3.h",
+		"backends/imgui_impl_opengl3.cpp",
+		"backends/imgui_impl_win32.cpp",
+		"backends/imgui_impl_win32.h",
+		"backends/imgui_impl_win32.cpp",
+	}
+
+	includedirs
+    {
+     "%{prj.name}",
+	 "%{prj.name}/backends"
+    }
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
